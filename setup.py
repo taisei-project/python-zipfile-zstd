14c76000f499701e82f55bf9c7b6cc37836e9ec1
Andrei Alexeyev <akari@taisei-project.org>
2021-03-28 14:21:38 +0300

    setup: bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='zipfile-zstd',
-    version="0.0.1",
+    version="0.0.2",
     author='Andrei Alexeyev',
     author_email='akari@taisei-project.org',
     description='Monkey patch the standard zipfile module to enable Zstandard support',
